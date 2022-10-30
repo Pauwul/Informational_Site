db23824db3f989907703a07479f3084866b8fa42
Pauwul <petrila.paulcosmin@gmail.com>
2022-10-30 22:01:43 +0200

    Add ejs files and objects

--- ExpressApp.js
+++ ExpressApp.js
@@ -5,6 +5,14 @@ const express = require("express");
 // express app
 const app = express();
 
+
+// register view engine
+
+app.set('view engine', 'ejs'); // defaults to views folder
+// alternatively, for custome view folder:
+// app.set('views', 'myviews');
+
+
 // this returns an instance of  the server,
 // could be useful for web sockets
 
@@ -16,7 +24,16 @@ app.get("/", (req, res) => {
   // this also sets the content type header
   //res.send("<p> home page </p>");
 
-  res.sendFile("./views/index.html", { root: __dirname });
+  //res.sendFile("./views/index.html", { root: __dirname });
+
+  // for ejs
+  const blogs = [
+    {title: "Mario runs over Yoshi with his Toyota ", snippet:""},
+    {title:"Yoda's Ketamine Addiction", snippet:"Yoda is high on Ketamine and needs your donations to fund his addiction"},
+    {title:"Terry the Fat Shark is back", snippet:"This week, Terryhas brought everyone a very plush thermal pillow!  This pillow is not only durable, but also always stays cold on both sides! Terry knows some people can't sleep very easily, or just hate turning pillows over to keep cool, so Terry hopes everyone can use this gift from now on to sleep comfortably whenver they want. Terry will be back wuth another gift next Wednesday"}
+  ]
+  res.render("index", {title: 'Home', blogs})
+
 
   /* the nodejs way
     res.setHeader('Content-Type', 'text/html')
@@ -28,17 +45,20 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   // res.send("<p>Heloo, this is about page</p>");
-  res.sendFile("/views/about.html", { root: __dirname });
+
+  // res.sendFile("/views/about.html", { root: __dirname });
+  res.render('about',{title:'About'})
 });
 
-// redirects
-
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
+app.get('/blogs/create', (req,res)=>{
+  res.render("create", { title: 'Create a new blog'})
+}) 
 
 // 404
 
 app.use((req, res) => {
-  res.sendFile("./views/404.html", { root: __dirname });
+  // res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404)
+  res.render('404', {title : '404'})
+  
 });
---
+++ README.md
@@ -0,0 +1,8 @@
+# Informational website
+
+## What I've learned
+
+- Basic Nodejs file sistem, routing and servers
+- Requests and Responses
+- How to work with express  
+- The ejs view engine
